59e721aff336b5670885c0d6eb36dc0d250cf990
SebastianMerino <sebastian.merino@pucp.edu.pe>
2024-06-12 17:14:10 -0500

    hamming window

--- AttenuationUtils/getIdealAcsInc.m
+++ AttenuationUtils/getIdealAcsInc.m
@@ -5,10 +5,12 @@ function attFinal = getIdealAcsInc(center,rInc,acsBack,acsInc,x_ACS,z_ACS,window
 %   rInc        inclusion radius in cm
 %   windowSize  2-array of window size in cm
 
-% cx = 1.85; cz = 1.93;
+% Example parameters
+% center = [1.85, 1.93];
 % rInc = 0.95;
 % acsBack = 0.5;
 % acsInc = 1;
+% windowSize = [blocksize blocksize*ratio_zx]*wl*100;
 
 % Ideal ACS dsitribution
 xUp = linspace(x_ACS(1), x_ACS(end),1000);
@@ -21,14 +23,18 @@ attUp(circle_inc) = acsInc;
 % Smoothing with blocksize
 nxDown = round(windowSize(1)/(xUp(2) - xUp(1)));                 % Window size
 nzDown = round(windowSize(2)/(zUp(2) - zUp(1))); % Window size
-attSmooth = imfilter(attUp,ones([nzDown nxDown])/nzDown/nxDown, ...
-    "symmetric","same");
+h = hamming(nzDown).*hamming(nxDown)';
+% h = tukeywin(nzDown,0.25).*tukeywin(nxDown,0.25)';
+% h = ones(nzDown,nxDown);
+h = h./sum(h(:));
+attSmooth = imfilter(attUp,h,"symmetric","same");
 
 % Downsampling to original resolution
 [X,Z] = meshgrid(x_ACS,z_ACS);
 attFinal = interp2(Xup,Zup,attSmooth,X,Z);
 
-% figure, tiledlayout(1,3)
+% figure('Units','centimeters', 'Position',[5 5 18 6]), 
+% tiledlayout(1,3)
 % nexttile
 % imagesc(xUp,zUp, attUp, [0.3,1.2])
 % colormap turbo
