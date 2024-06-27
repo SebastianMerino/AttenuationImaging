595583b8fa8a46ccb82be6649ab93f099bf8d8dd
SebastianMerino <sebastian.merino@pucp.edu.pe>
2024-06-27 13:49:35 -0500

    modified focal simul. added scripts for liver

--- 24_06/testMultifocal.m
+++ 24_06/testMultifocal.m
@@ -1,13 +1,36 @@
+clear,clc
+
+targetDir = ['C:\Users\sebas\Documents\MATLAB\DataProCiencia\' ...
+    'Attenuation\Simulation\24_06_26_inc'];
+refDir = ['C:\Users\sebas\Documents\MATLAB\DataProCiencia\' ...
+    'Attenuation\Simulation\24_06_26_ref'];
+resultsDir = fullfile(targetDir,'results');
+
+[~,~] = mkdir(resultsDir);
+targetFiles = dir([targetDir,'\rf*.mat']);
+% refFiles = dir([refDir,'\rf*.mat']);
+% tableName = 'simuInc.xlsx';
+
+%%
+iAcq = 2;
+load(fullfile(targetDir,targetFiles(iAcq).name));
+x = x*100; z = z*100;
+
 Bmode1 = getBmode(rf(:,:,1));
 Bmode2 = getBmode(rf(:,:,2));
 Bmode3 = getBmode(rf(:,:,3));
 dynRange = [-50 0];
 
 %%
-Bmode = getBmode(mean(rf,3));
+[~,Z] = meshgrid(x,z);
+concatRf = rf(:,:,1).*(Z<1.5) + rf(:,:,2).*(Z>=1.5 & Z<2.5) + ...
+    rf(:,:,3).*(Z>=2.5);
+meanRf = mean(rf,3);
 
+meanBm = getBmode(meanRf);
+concatBm = getBmode(concatRf);
 
-figure,tiledlayout(1,4)
+figure,tiledlayout(2,3)
 nexttile,
 imagesc(x,z,Bmode1, dynRange)
 colormap gray
@@ -24,10 +47,58 @@ colormap gray
 axis image
 
 nexttile,
-imagesc(x,z,Bmode, dynRange)
+imagesc(x,z,meanBm, dynRange)
 colormap gray
 axis image
 
+nexttile,
+imagesc(x,z,concatBm, dynRange)
+colormap gray
+axis image
+
+%%
+figure,
+plot(z,mean(concatBm(:,46:50),2))
+hold on
+plot(z,mean(meanBm(:,46:50),2))
+grid on
+axis tight
+
+%% Lines
+ix = 15;
+figure('Units','centimeters', 'Position',[5 5 30 15]),
+tiledlayout(3,1)
+nexttile,
+plot(z,rf(:,ix,1))
+grid on
+axis tight
+xlim([z(1), z(end)])
+ylim([-1,1]*1e4)
+
+nexttile,
+plot(z,rf(:,ix,2))
+grid on
+axis tight
+xlim([z(1), z(end)])
+ylim([-1,1]*1e4)
+
+nexttile,
+plot(z,rf(:,ix,3))
+grid on
+xlim([z(1), z(end)])
+ylim([-1,1]*1e4)
+%%
+figure('Units','centimeters', 'Position',[5 5 30 15]),
+
+plot(z,concatRf(:,ix))
+% grid on
+% axis tight
+% 
+% nexttile,
+hold on
+plot(z,meanRf(:,ix))
+grid on
+axis tight
 
 %%
 function bm = getBmode(rf)
